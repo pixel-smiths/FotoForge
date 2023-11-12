712c9b8e871682c3248d16ebd68eb679359dcebf
SKel1817 <vnielson@oakland.edu>
2023-11-12 16:13:25 -0500

    PateClipboard, and upload are working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import pygame
+import Fotoforge
 
 # Initialize Pygame
 pygame.init()
@@ -13,8 +14,22 @@ screen = pygame.display.set_mode(window_size)
 # Set the title of the window
 pygame.display.set_caption("FotoForge")
 
-#set backgorund to white
+# Set background color
 screen.fill((255, 255, 255))
+
+# Define button properties
+button_color = (0, 255, 0)
+button_text = "New From Image"
+button_font = pygame.font.Font(None, 36)
+
+# Create button surface
+button_surface = button_font.render(button_text, True, button_color)
+
+# Get button surface rectangle
+button_rect = button_surface.get_rect()
+
+# Draw button on screen
+screen.blit(button_surface, button_rect)
 
 # Run the game loop
 while True:
@@ -24,6 +39,14 @@ while True:
             # Quit the game if the user closes the window
             pygame.quit()
             exit()
-
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Check if the button was clicked
+            if button_rect.collidepoint(event.pos):
+                # Call the openFromImage() function
+                Fotoforge.newFromImage(screen)
+        #if the keypress is ctrl+v, paste the clipboard
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_v and pygame.key.get_mods() & pygame.KMOD_CTRL:
+                Fotoforge.PasteClipboard(screen)
     # Update the screen
     pygame.display.update()
