af9ea3bdc5b514aaa0c5730b049ff59b18e1ce93
sdziobak <sarahdziobak@gmail.com>
2023-11-14 11:31:31 -0500

    add layers broken

--- LayerManager.py
+++ LayerManager.py
@@ -2,227 +2,139 @@ import os
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import pygame
-from PIL import Image
-from tkinter import Tk
-from PIL import ImageGrab
 
-class LayerManager:
-    # Add these lines after creating the LayerManager instance
-      
- 
-    def __init__(self, surface):
-        self.surface = surface
-        self.layers = []
-        self.active_layer = None
-        self.opacity_slider_value = 100  # Default opacity value
-
-    # def add_layer(self):
-    #     layer = Layer(self.surface)
-    #     self.layers.append(layer)
-    #     self.active_layer = layer
-    def add_layer(self):
-        layer = Layer(self.surface)
-        self.layers.append(layer)
-        self.active_layer = layer
-
-        # Clear the layer
-        self.active_layer.clear_layer()
-
-        # Blit all images onto the new layer with opacity
-        for (image, opacity), rect in zip(self.active_layer.images, self.active_layer.rects):
-            # Create a copy of the image with the desired opacity
-            alpha_surface = pygame.Surface(image.get_size(), pygame.SRCALPHA)
-            alpha_surface.fill((255, 255, 255, opacity))
-            image_copy = pygame.Surface(image.get_size(), pygame.SRCALPHA)
-            image_copy.blit(alpha_surface, (0, 0))
-            image_copy.blit(image, (0, 0))
-
-            # Blit the image onto the new layer
-  # Clear the layer
-        self.surface.fill((255, 255, 255))
-
-        # Blit all images onto the layer with opacity
-        for (image, opacity), rect in zip(self.images, self.rects):
-            # Create a copy of the image with the desired opacity
-            alpha_surface = pygame.Surface(image.get_size(), pygame.SRCALPHA)
-            alpha_surface.fill((255, 255, 255, opacity))
-            image_copy = pygame.Surface(image.get_size(), pygame.SRCALPHA)
-            image_copy.blit(alpha_surface, (0, 0))
-            image_copy.blit(image, (0, 0))
-
-            # Blit the image onto the layer
-            self.surface.blit(image_copy, rect)
-    def update_opacity_slider(self, new_value):
-        # Clamp the new value between 0 and 100
-        self.opacity_slider_value = max(0, min(new_value, 100))
-
-    def upload_image_to_active_layer(self):
-        if self.active_layer is not None:
-            # Set the caption of the Pygame window to "Select an Image"
-            pygame.display.set_caption("Select an Image for Layer")
-
-            # Open a file system dialog to select an image
-            Tk().withdraw()
-            filename = askopenfilename()
-
-            # Load the selected image
-            image = pygame.image.load(filename)
-
-            # Resize image to keep sizing right but to be 100 pixels less than the screen and centered on the screen
-            #make image to be it's orginal size but also fit in the window
-            #image = pygame.transform.scale(image, (self.surface.get_width() - 50, self.surface.get_height() - 50))
-
-            #image = pygame.transform.scale(image, (self.surface.get_width() - 250, self.surface.get_height() - 250))
-            # Center image on the active layer
-            image_rect = image.get_rect()
-            image_rect.center = (self.surface.get_width() / 2, self.surface.get_height() / 2)
-
-            # Set the caption of the Pygame window to the name of the selected image
-            pygame.display.set_caption(os.path.basename(filename))
-
-            # Update the Pygame window
-            pygame.display.update()
-
-            # Blit the loaded image onto the active layer
-            self.active_layer.blit_image(image, image_rect)
-
-             # Center image on the active layer without resizing
-            image_rect.center = self.active_layer.surface.get_rect().center
-
-            # Blit the loaded image onto the active layer
-            self.active_layer.blit_image(image, image_rect)
-
-            # Update the original Pygame surface
-            pygame.display.update()
-            #   # Blit the loaded image onto the active layer
-            # self.active_layer.blit_image(image, image_rect)
-
-            # # Update the original Pygame surface
-            # pygame.display.update()
-
-            # def move_image(image_rect, direction, pixels):
-            #   if direction == "up":
-            #    image_rect.y -= pixels
-            #   elif direction == "down":
-            #    image_rect.y += pixels
-            #   elif direction == "left":
-            #     image_rect.x -= pixels
-            #   elif direction == "right":
-            #     image_rect.x += pixels
-
-
-# class Layer:
-#     def __init__(self, surface):
-#         self.surface = surface
-#         self.image = None
-
-#     def blit_image(self, image, rect):
-#         self.image = image
-#         self.rect = rect
-#         self.surface.blit(self.image, self.rect)
-# class Layer:
-#     def __init__(self, surface):
-#         self.surface = surface
-#         self.images = []  # Store a list of images
-#         self.rects = []   # Store a list of corresponding rectangles
-
-#     def blit_image(self, image, rect):
-#         self.images.append(image)
-#         self.rects.append(rect)
-#         self.update_layer()
-
-#     def update_layer(self):
-#         self.surface.fill((255, 255, 255))  # Clear the layer
-#         for image, rect in zip(self.images, self.rects):
-#             self.surface.blit(image, rect)
-# class Layer:
-#     def __init__(self, surface):
-#         self.surface = surface
-#         self.images = []  # Store a list of images
-#         self.rects = []   # Store a list of corresponding rectangles
-
-#     def blit_image(self, image, rect):
-#         # Store the original size of the image
-#         original_size = image.get_size()
-
-#         # Create a new rectangle for the image based on its original size
-#         new_rect = pygame.Rect(rect.topleft, original_size)
-
-#         # Append the new image and rectangle to the lists
-#         self.images.append(image)
-#         self.rects.append(new_rect)
-
-#         # Update the layer
-#         self.update_layer()
-
-#     def update_layer(self):
-#         self.surface.fill((255, 255, 255))  # Clear the layer
-#         for image, rect in zip(self.images, self.rects):
-#             self.surface.blit(image, rect)
-# class Layer:
-#     def __init__(self, surface):
-#         self.surface = surface
-#         self.images = []  # Store a list of images
-#         self.rects = []   # Store a list of corresponding rectangles
-
-#     def blit_image(self, image, rect):
-#         # Store the original size of the image
-#         original_size = image.get_size()
-
-#         # Create a new rectangle for the image based on its original size
-#         new_rect = pygame.Rect(rect.topleft, original_size)
-
-#         # Append the new image and rectangle to the lists
-#         self.images.append(image)
-#         self.rects.append(new_rect)
-
-#     def update_layer(self):
-#         # Clear the layer
-#         self.surface.fill((255, 255, 255))
-        
-#         # Blit all images onto the layer
-#         for image, rect in zip(self.images, self.rects):
-#             self.surface.blit(image, rect)
 class Layer:
     def __init__(self, surface):
         self.surface = surface
         self.images = []  # Store a list of images
         self.rects = []   # Store a list of corresponding rectangles
+        
+
+        self.surface.set_alpha(10)
+        # Other initialization code...
+
+    # def blit_image(self, image, rect):
+    #     # Append the new image and rectangle to the lists
+    #     self.images.append(image)
+    #     self.rects.append(rect)
 
     def blit_image(self, image, rect):
-        # Append the new image and rectangle to the lists
+        image = image.convert_alpha()  # Ensure the image supports per-pixel alpha
+        image.fill((255, 255, 255, 128), special_flags=pygame.BLEND_RGBA_MULT)  # Set the alpha value of the image
         self.images.append(image)
         self.rects.append(rect)
 
     # def update_layer(self):
-    #     # Clear the layer
-    #     self.surface.fill((255, 255, 255))
-        
-    #     # Blit all images onto the layer
+    # # If there are images to blit onto the layer, fill the layer with a fully transparent color
+    #     if self.images:
+    #         self.surface.fill((0, 0, 0, 0))
+    # # If there are no images, fill the layer with a fully opaque white color
+    #     else:
+    #        self.surface.fill((255, 255, 255))
+
+    # # Blit all images onto the layer
     #     for image, rect in zip(self.images, self.rects):
     #         self.surface.blit(image, rect)
+
     def update_layer(self):
         # Clear the layer
-        self.surface.fill((255, 255, 255))
+        self.surface.fill((250,250,250,250))
 
-        # Blit all images onto the layer with opacity
-        for (image, opacity), rect in zip(self.images, self.rects):
-            # Create a copy of the image with the desired opacity
-            alpha_surface = pygame.Surface(image.get_size(), pygame.SRCALPHA)
-            alpha_surface.fill((255, 255, 255, opacity))
-            image_copy = pygame.Surface(image.get_size(), pygame.SRCALPHA)
-            image_copy.blit(alpha_surface, (0, 0))
-            image_copy.blit(image, (0, 0))
+        # Blit all images onto the layer
+        for image, rect in zip(self.images, self.rects):
+            self.surface.blit(image, rect)
+# class Layer:
+#     def __init__(self, surface):
+#         self.surface = pygame.Surface(surface.get_size(), pygame.SRCALPHA)  # Create a surface that supports per-pixel alpha
+#         self.images = []
+#         self.rects = []
 
-            # Blit the image onto the layer
-            self.surface.blit(image_copy, rect)
+#     def blit_image(self, image, rect):
+#         image = image.convert_alpha()  # Ensure the image supports per-pixel alpha
+#         self.images.append(image)
+#         self.rects.append(rect)
 
-    def blit_image(self, image, rect):
-        # Append the new image, rectangle, and opacity to the lists
-        opacity = layer_manager.opacity_slider_value
-        self.images.append((image, opacity))
-        self.rects.append(rect)
+#     def update_layer(self):
+#         self.surface.fill((0, 0, 0, 0))  # Fill the layer with fully transparent color
 
+#         for image, rect in zip(self.images, self.rects):
+#             self.surface.blit(image, rect)
+
+
+class LayerManager:
+    def __init__(self, surface):
+        self.surface = surface
+        self.layers = []
+        self.active_layer = None
+
+    def add_layer(self):
+        layer = Layer(self.surface)
+        self.layers.append(layer)
+        self.active_layer = layer
+    # def add_layer(self):
+    #     width, height = self.surface.get_size()
+    #     new_layer = Layer(width, height)
+    #     self.layers.append(new_layer)
+    #     self.active_layer = new_layer
+
+    def add_layer_with_image(self, file_path):
+        self.add_layer()  # create a new layer and set it as the active layer
+        self.upload_image_to_active_layer(file_path)  # upload the image to the active layer
+
+    # def upload_image_to_active_layer(self, file_path):
+    #     if self.active_layer is not None:
+    #         image = pygame.image.load(file_path)
+
+    #         image_rect = image.get_rect()
+    #         image_rect.center = (self.surface.get_width() / 2, self.surface.get_height() / 2)
+
+    #         self.active_layer.blit_image(image, image_rect)
+
+    # def upload_image_to_active_layer(self):
+    #     if self.active_layer is not None:
+    #         pygame.display.set_caption("Select an Image for Layer")
+    #         Tk().withdraw()
+    #         filename = askopenfilename()
+    #         image = pygame.image.load(filename)
+
+    #         image_rect = image.get_rect()
+    #         image_rect.center = (self.surface.get_width() / 2, self.surface.get_height() / 2)
+
+    #         pygame.display.set_caption(os.path.basename(filename))
+    #         pygame.display.update()
+
+    #         self.active_layer.blit_image(image, image_rect)
+    #         pygame.display.update()
+    def upload_image_to_active_layer(self):
+      if self.active_layer is not None:
+           pygame.display.set_caption("Select an Image for Layer")
+           Tk().withdraw()
+           filename = askopenfilename()
+           image = pygame.image.load(filename)
+
+        # Get the size of the image and the window
+           image_width, image_height = image.get_size()
+           window_width, window_height = self.surface.get_size()
+
+        # Calculate the scaling factors
+           width_scale = window_width / image_width
+           height_scale = window_height / image_height
+
+        # Choose the smaller scaling factor
+           scale = min(width_scale, height_scale)
+
+        # Scale the image
+           if scale < 1.0:  # only scale down, not up
+              image = pygame.transform.scale(image, (int(image_width * scale), int(image_height * scale)))
+
+           image_rect = image.get_rect()
+           image_rect.center = (window_width / 2, window_height / 2)
+
+           pygame.display.set_caption(os.path.basename(filename))
+           pygame.display.update()
+
+           self.active_layer.blit_image(image, image_rect)
+           pygame.display.update()
 
 
 
